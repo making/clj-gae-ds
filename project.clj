f737dac5882613496852a892cb27865325587e4f
making <makingx@gmail.com>
2010-03-30 03:46:07 +0900

    move appengine-testing to dev-dependencies

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
                  "maven.seasar.org" "http://maven.seasar.org/maven2"}
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
-                 [com.google.appengine/appengine-api-1.0-sdk "1.3.1"]
-                 [com.google.appengine/appengine-testing "1.3.1"]]
-  :dev-dependencies [[com.google.appengine/appengine-api-labs "1.3.1"]
+                 [com.google.appengine/appengine-api-1.0-sdk "1.3.1"]]
+  :dev-dependencies [[com.google.appengine/appengine-testing "1.3.1"]
+                     [com.google.appengine/appengine-api-labs "1.3.1"]
                      [com.google.appengine/appengine-api-1.0-stubs "1.3.1"]                     
                      ;; [com.google.appengine/appengine-api-1.0-runtime "1.3.1"]
                      ;; [com.google.appengine/appengine-tools-sdk "1.3.1"]
@@ -14,5 +14,4 @@
                      ;; [leiningen/lein-swank "1.1.0"]
                      [lein-clojars "0.5.0-SNAPSHOT"]
                      ] 
-  :namespaces [am.ik.clj-gae-ds.core
-               am.ik.clj-gae-ds.test-utils])
+  :namespaces [am.ik.clj-gae-ds.core])
