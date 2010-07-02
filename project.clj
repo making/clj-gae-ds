3cce5922444b0ac2f3238f3901ebeffa0a1ca3db
Toshiaki Maki <makingx@gmail.com>
2010-07-02 11:58:50 -0700

    update GAE SDK to 1.3.5

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject am.ik/clj-gae-ds "0.2.0"
+(defproject am.ik/clj-gae-ds "0.2.1"
   :description "a datastore library on Google App Engine for Clojure"
   :repositories {"maven.seasar.org" "http://maven.seasar.org/maven2",}
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
-                 [com.google.appengine/appengine-api-1.0-sdk "1.3.4"]]
+                 [com.google.appengine/appengine-api-1.0-sdk "1.3.5"]]
   :dev-dependencies [[am.ik/clj-gae-testing "0.2.0-SNAPSHOT"]
                      ;;[leiningen/lein-swank "1.1.0"]
                      [lein-clojars "0.5.0-SNAPSHOT"]]
