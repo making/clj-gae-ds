ebcb11709b4d1ddf12d3fa752c5322bec01996c2
Jan Stępień <jstepien@users.sourceforge.net>
2011-06-28 12:49:16 +0200

    Updated Clojure to 1.2.1

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject am.ik/clj-gae-ds "0.3.0-SNAPSHOT"
   :description "a datastore library on Google App Engine for Clojure"
   :repositories {"maven.seasar.org" "http://maven.seasar.org/maven2",}
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [com.google.appengine/appengine-api-1.0-sdk "1.5.1"]]
   :dev-dependencies [[am.ik/clj-gae-testing "0.2.0-SNAPSHOT"]
